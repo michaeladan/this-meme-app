d41973e4496d044c559cbc27da2995dad34059ce
Michael Adan <electric_boarder@hotmail.com>
2022-10-17 11:31:51 -0600

    Initial commmit - completed project.

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React - API</h4>
+        </header>
+    )
+}
